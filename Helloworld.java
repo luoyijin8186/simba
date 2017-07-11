9e2aeee6df8d89d34ef5ec9e52073d206fbe58e4
wjl120 <wjl120wjl@163.com>
2017-07-11 11:53:27 +0800

    1

--- Helloworld.java
+++ Helloworld.java
@@ -7,7 +7,7 @@ public class Helloworld extends Thread {
             System.out.print(i);
             try {
                 Thread.sleep(1000);
-                System.out.print("Hello world！\n");
+                System.out.print("Hello world!\n");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
