150fd83a7b350ecdc09b77652a3a1efd1dd1ce68
luoyijin8186 <31915370+luoyijin8186@users.noreply.github.com>
2017-09-13 15:17:51 +0800

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,18 +1,8 @@
 public class Helloworld extends Thread {
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            if ((i) % 10 == 0) {
-                System.out.println("-------" + i);
-            }
-            System.out.print(i);
-            try {
-                Thread.sleep(1000);
-                System.out.print("Hello world! by lyj\n");
-                
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        
+      System.out.print("Hello world! by lyj\n");
+ 
     }
     public static void main(String[] args) {
         new Helloworld().start();
